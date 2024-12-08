1763fc5eb667ed92f797b94d79bee68ada9661b4
Jay Shah <jay@jays-air.home>
2024-12-07 21:02:06 -0500

    Updating the main.ts file

--- Assignment_4/farmkart-backend/src/main.ts
+++ Assignment_4/farmkart-backend/src/main.ts
@@ -7,11 +7,10 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000','http://localhost:5173'], // Adjust this to your Next.js frontend URL
+    origin: [process.env.IONIC_URI,process.env.NEXT_URI],
     credentials: true,
   });
 
-  // for image upload
   app.use(bodyParser.json({
     limit : '10mb'
   }));
